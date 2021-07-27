99aca2522cac987945c557d6842e2aba8eafbbf5
JaymeeYX <87964859+JaymeeYX@users.noreply.github.com>
2021-07-27 22:01:24 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,94 @@
+basic.forever(function () {
+    music.playMelody("F G A - B A B - ", 120)
+    music.playMelody("B A B C5 - B A B ", 120)
+    music.playMelody("G A B - B A B - ", 120)
+    music.playMelody("A G A B - G F G ", 120)
+    music.playMelody("- - - - - - - - ", 120)
+    music.playMelody("- - - - - - - - ", 120)
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . # # # .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . # # # .
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . # . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
